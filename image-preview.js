b6d845d66b8cc7c904759384e1d3bc915a1a6834
Islam H ALAlfy <61562847+EngALAlfy@users.noreply.github.com>
2023-06-24 20:00:41 +0300

    Update image-preview.js

--- image-preview.js
+++ image-preview.js
@@ -1,16 +1,42 @@
-// Zoom preview functionality
-const imagesPreview = document.querySelectorAll('.image-previewed');
+// Initialize Image Preview
+function initializeImagePreview() {
+    // Get all elements with the 'image-previewed' class
+    const imageElements = document.querySelectorAll('.image-previewed');
 
-imagesPreview.forEach((image) => {
-    image.addEventListener('click', () => {
-        const zoomPreview = document.querySelector('.zoom-preview');
-        const zoomedImg = zoomPreview.querySelector('.zoomed-image');
-        zoomedImg.src = image.src;
-        zoomPreview.classList.add('active');
+    // Add click event listener to each image element
+    imageElements.forEach((imageElement) => {
+        imageElement.addEventListener('click', () => {
+            // Create a zoomed-in preview of the clicked image
+            createImagePreview(imageElement);
+        });
     });
-});
+}
 
-const zoomPreview = document.querySelector('.zoom-preview');
-zoomPreview.addEventListener('click', () => {
-    zoomPreview.classList.remove('active');
-});
+// Create Image Preview
+function createImagePreview(imageElement) {
+    // Get the source and alt attributes of the clicked image
+    const imageSrc = imageElement.getAttribute('src');
+    const imageAlt = imageElement.getAttribute('alt');
+
+    // Create a modal container for the image preview
+    const modalContainer = document.createElement('div');
+    modalContainer.classList.add('image-preview-modal');
+
+    // Create the image element for the preview
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('src', imageSrc);
+    previewImage.setAttribute('alt', imageAlt);
+    previewImage.classList.add('preview-image');
+
+    // Append the image element to the modal container
+    modalContainer.appendChild(previewImage);
+
+    // Append the modal container to the body
+    document.body.appendChild(modalContainer);
+
+    // Add click event listener to the modal container to close the preview
+    modalContainer.addEventListener('click', () => {
+        // Remove the modal container from the DOM
+        modalContainer.remove();
+    });
+}
