ec0bd23a90fcb99a99195c5982d1a890d8b6ff77
Islam H ALAlfy <61562847+EngALAlfy@users.noreply.github.com>
2023-06-24 20:34:27 +0300

    Update image-preview.js

--- image-preview.js
+++ image-preview.js
@@ -20,23 +20,27 @@ function createImagePreview(imageElement) {
 
     // Create a modal container for the image preview
     const modalContainer = document.createElement('div');
-    modalContainer.classList.add('image-preview-modal');
+    modalContainer.classList.add('zoom-preview');
 
     // Create the image element for the preview
     const previewImage = document.createElement('img');
     previewImage.setAttribute('src', imageSrc);
     previewImage.setAttribute('alt', imageAlt);
-    previewImage.classList.add('preview-image');
+    previewImage.classList.add('zoomed-image');
 
     // Append the image element to the modal container
     modalContainer.appendChild(previewImage);
-
+    modalContainer.classList.add("active");
     // Append the modal container to the body
     document.body.appendChild(modalContainer);
 
     // Add click event listener to the modal container to close the preview
     modalContainer.addEventListener('click', () => {
         // Remove the modal container from the DOM
-        modalContainer.remove();
+        modalContainer.classList.remove("active");
+        // Remove the modal container from the DOM after the animation completes
+        setTimeout(() => {
+            modalContainer.remove();
+        }, 300); // Adjust the delay (in milliseconds) to match the animation duration
     });
 }
